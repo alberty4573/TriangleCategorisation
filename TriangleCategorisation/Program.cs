0b568e56d458b34ba43403f10f418cdf033730ab
alberty4573 <shenlieku@gmail.com>
2021-03-09 14:50:30 +1100

    Initial check-in of module TriangleCategorisation

---
+++ TriangleCategorisation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TriangleCategorisation
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                // Input three sides of the triangle
+                Console.WriteLine("Please type in the first side of a triangle: ");
+                var triSide1 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Please type in the second side of a triangle: ");
+                var triSide2 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Please type in the third side of a triangle: ");
+                var triSide3 = Convert.ToDecimal(Console.ReadLine());
+
+                // Could use DI here
+                var triangle = new Triangle();
+                if (triangle.isTriangle(triSide1, triSide2, triSide3))
+                {
+                    Console.WriteLine("This is a " + triangle.catTriangle(triSide1, triSide2, triSide3)); 
+                } else
+                {
+                    Console.WriteLine("This is not a triangle");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Here are the error message: " + ex);
+
+            }
+
+            
+        }
+    }
+}
---
+++ TriangleCategorisation/Triangle.cs
@@ -0,0 +1,49 @@
+﻿namespace TriangleCategorisation
+{
+    class Triangle
+    {
+        public string type { get; set; }
+
+        // Judge if its a triangle
+        public bool isTriangle(decimal side1, decimal side2, decimal side3)
+        {
+            bool isTriangleVar = false;
+            if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
+            {
+                isTriangleVar = true;
+            }
+
+            return isTriangleVar;
+        }
+
+        // Categorise different triangles
+        public string catTriangle(decimal side1, decimal side2, decimal side3)
+        {
+            string result1 = "";
+            string result2 = "";
+
+            if (side1 == side2 || side2 == side3 || side1 == side3)
+            {
+                if (side1 == side2 && side1 == side3)
+                {
+                    return result1 = "Equilateral Triangle";
+                }
+
+                result1 = "Isosceles Triangle";
+            }
+
+            // This result will not be printed since there is no suitable input
+            if (side1 * side1 + side2 * side2 == side3 * side3 || side1 * side1 + side3 * side3 == side2 * side2 || side2 * side2 + side3 * side3 == side1 * side1)
+            {
+                result2 = "Right Triangle";
+            }
+
+            if (result1 == "" && result2 == "")
+            {
+                return "Normal Triangle";
+            }
+
+            return result1 + result2;
+        }
+    }
+}
